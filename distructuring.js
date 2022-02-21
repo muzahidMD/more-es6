e6a2f6cb1add05386c3dd0761991a8e0d173e21f
Muzahidul Islam <mujahidul.core@gmail.com>
2022-02-21 23:03:41 +0600

    simple changes

--- distructuring.js
+++ distructuring.js
@@ -6,12 +6,12 @@ const fish = { id: 58, name: 'King Hilsha', price: 9000, phone: '017505454545', 
 const { phone, price, dress, id } = fish;
 
 
-console.log(phone, price, dress, id);
-console.log(phone, dress);
-console.log(phone, id);
-console.log(phone, price);
-console.log(phone, id);
-console.log(phone, dress);
+// console.log(phone, price, dress, id);
+// console.log(phone, dress);
+// console.log(phone, id);
+// console.log(phone, price);
+// console.log(phone, id);
+// console.log(phone, dress);
 
 const company = {
     name: 'Gp',
@@ -28,5 +28,12 @@ const company = {
     }
 }
 
-console.log(company.web.framework)
-console.log(company.backend.tech.third)
+// const work = company.web.Work;
+// const framework = company.web.framework;
+const tech = company.web.tech.first;
+const { work, framework } = company.web;
+const { food } = company.ceo;
+const { second, third } = company.web.tech;
+
+// console.log(company.web.framework)
+// console.log(company.backend.tech.third)
